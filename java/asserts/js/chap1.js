6b84876f1bd8334157bc4217490a789d97b9bd51
AmnaNehal <128274838+AmnaNehal@users.noreply.github.com>
2023-07-04 00:26:27 +0500

    Add files via upload

---
+++ java/asserts/js/chap1.js
@@ -0,0 +1,14 @@
+                                    //QUESTION NO: 01
+
+alert("Welcome to the website");
+                                    //QUESTION NO: 02
+alert("Please enter a valid password");
+                                    //QUESTION NO: 03
+alert("Welcome to JS Land....\nHappy Coding!")
+                                    //QUESTION NO: 05
+alert("Welcome to JS Land....");
+alert("Happy Coding!\n\nPrevent this page from creating additional dialogs");
+                                    //QUESTION NO: 06
+console.log("Hi...i can run JS through my web browser's console");
+                                    //QUESTION NO: 04 & 07
+                                    //DONE PRACTICES
---
+++ java/asserts/js/chap2.js
@@ -0,0 +1,25 @@
+                                    //QUESTION NO: 01
+var userName
+                                    //QUESTION NO: 02
+var myName="Amna Nehal"
+                                    //QUESTION NO: 03
+var message="Hello World";
+alert(message); 
+                                    //QUESTION NO: 04
+var stuName="Jhone Doe" ; 
+alert(stuName);
+var stuAge="15 years old";
+alert(stuAge);
+var meSsage="Certifite Mobile Application Development";
+alert(meSsage);
+                                    //QUESTION NO: 05
+var piZza="PIZZA\nPIZZ\nPIZ\nPI\nP"  ;
+alert(piZza);     
+                                    //QUESTION NO: 06
+var email="amna@nehal.com" ; 
+alert("My email address is "+email);
+                                    //QUESTION NO: 07
+var book="A smarter way to learn JavaScript"   
+alert("I am trying to learn from the book "+book);
+                                     //QUESTION NO: 08
+ document.write("Yeah! I can write HTML content through JavaScript")                                                       
